857e28bd1524a03da5e388fd41f47bcb11513ab5
Youssef Gabriel Amazirh <74822145+usrmny@users.noreply.github.com>
2025-02-25 22:31:09 -0400

    Add files via upload

---
+++ naUniversities.java
@@ -0,0 +1,11 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class naUniversities {
+    public static void main(String[] args){
+        Class.forName
+    }
+}
---
+++ northamericauniversities.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `northamericauniversities` (
+  `Rank` int DEFAULT NULL,
+  `Name` text,
+  `Country` text,
+  `Established` int DEFAULT NULL,
+  `Academic Staff` text,
+  `Number of Students` int DEFAULT NULL,
+  `Minimum Tuition cost` text,
+  `Volumes in the library` int DEFAULT NULL,
+  `Endowment` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
